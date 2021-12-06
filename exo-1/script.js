99a545978ed5d8302f370ecde83dcdbd56aa983d
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2021-12-06 16:24:34 +0100

    exercice 1 OK

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,13 @@
 // you can write js here
-console.log('hello from file');
+const kelvin = prompt("Quelle est la température en Kelvin aujourd'hui ?");
+
+// on initialise la constante celsius avec une valeur
+const celsius = kelvin - 273;
+
+// on initialise la constante fahrenheit avec une valeur
+let fahrenheit = celsius * (9/5) + 32;
+
+// on génère le plus grand entier inférieur ou égal
+fahrenheit = Math.floor(fahrenheit);
+
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit`);
