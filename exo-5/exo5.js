a54125ebf29ca17442d1d733d87538ef44b57577
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2021-12-07 16:24:45 +0100

    exo 5 OK

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,17 @@
 // you can write js here
 
 console.log('exo-5');
+
+let input = "Pas de consonnes, uniquement les voyelles, pas d'espace et toutes les lettres en majuscules";
+input = input.toUpperCase();
+let vowels = ['A','E','I','O','Y','U'];
+let resultArray = [];
+
+for (let test of input) {
+    console.log(test);
+    if(vowels.indexOf(test) !== -1) {
+        resultArray.push(test);
+    }
+}
+
+console.log(resultArray.join(" "));
