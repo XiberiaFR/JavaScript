39bafb9211a8c01e881c447c8287c1798f2d110b
XiberiaFR <62890250+XiberiaFR@users.noreply.github.com>
2021-12-07 09:44:55 +0100

    exo 2 OK

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,25 @@
 // you can write js here
-console.log('exo-2');
+let statutInscription;
+let ageCoureur;
+
+function inscriptionCoureur(statutInscription, ageCoureur) {
+    var raceNumber = Math.floor(Math.random()*1000);
+    if(!statutInscription) {
+    raceNumber += 1000;
+    console.log('Votre numéro de dossard est le ' + raceNumber);
+    }
+    if(statutInscription && ageCoureur > 18) {
+        console.log('You will race at 9:30am');
+    }
+    else if(statutInscription || ageCoureur > 18) {
+        console.log('You will race at 11:00 am. ' + 'Votre numéro de dossar est le ' + raceNumber);
+    }
+    else if(ageCoureur < 18 || !statutInscription) {
+        console.log('You will race at 12:30');
+    }
+    else {
+        console.log("Go to see the registration desk");
+    }
+}
+
+inscriptionCoureur(true, 19);
